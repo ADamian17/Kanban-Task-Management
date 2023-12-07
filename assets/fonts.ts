ed2032575f91fc7a2674ddfc027e874ded89b0d3
ADamian17 <adonisdesuvikdo@gmail.com>
2023-12-06 20:36:44 -0800

    added color and typography

---
+++ assets/fonts.ts
@@ -0,0 +1,7 @@
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: ["200", "300", "400", "500", "600", "700","800"],
+  subsets: ['latin'],
+  display: 'swap',
+})
