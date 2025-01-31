4744898a5931529ccd6f00ab9f2a1f2f25cf7925
ADamian17 <adonismartin90@gmail.com>
2025-01-30 20:36:18 -0800

    updated EmptyBoard

--- components/layouts/BoardLayout/EmptyBoard/index.tsx
+++ components/layouts/BoardLayout/EmptyBoard/index.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+import Button from "@/components/ui/Button";
+import Modal from "@/components/ui/Modal";
+
+import styles from './EmptyBoard.module.scss';
 
 type EmptyBoardType = {
   boardId: string;
 };
 
 const EmptyBoard: React.FC<EmptyBoardType> = ({ boardId }) => {
+  const [show, setShow] = useState(false);
+
+  const handleOpen = () => {
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
   return (
-    <div>
-      <p>This board is empty. Create a new column to get started.</p>
-      Add New Column to board {boardId}
+    <div className={styles.wrapper}>
+      <p className={styles.headline}>This board is empty. Create a new column to get started.</p>
+
+      <Button onClick={handleOpen} className={styles.btn}>+ Add New Column</Button>
+
+      <Modal show={show} onClose={handleClose} title="Add New Column">
+        new column form {boardId}
+      </Modal>
     </div>
   );
 };
